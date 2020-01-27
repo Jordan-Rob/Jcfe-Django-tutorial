b60c05b2b095a6942189bb0a463de4fc454a5d81
jordanrob709 <jordanrob709@gmail.com>
2020-01-27 13:27:03 +0300

    blog templates

--- Blog/models.py
+++ Blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 
@@ -7,3 +8,6 @@ class Article(models.Model):
     name = models.CharField(max_length=120)
     article = models.TextField(blank=False, null=True)
     author = models.CharField(max_length=120)
+
+    def get_absolute_url(self):
+        return reverse("Blog:article", kwargs={"id": self.id})
