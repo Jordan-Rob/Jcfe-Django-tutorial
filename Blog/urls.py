3fdd21bd19130a33fe290ad703950a3f03df14ec
jordanrob709 <jordanrob709@gmail.com>
2020-01-27 18:08:06 +0300

    use of class based views

--- Blog/urls.py
+++ Blog/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 
-from .views import article_detail_view, article_list_view
+#from .views import article_detail_view, article_list_view, ArticlelistView
+from .views import ArticlelistView, ArticleDetailView
 
 app_name = 'Blog'
 urlpatterns = [
-    path('article/<int:id>/', article_detail_view, name='article'),
-    path('articles/', article_list_view, name='articles')
+    #path('article/<int:id>/', article_detail_view, name='article'),
+    #path('articles/', article_list_view, name='articles')
+    path('artcle/<int:pk>/', ArticleDetailView.as_view(), name='article'),
+    path('articles/', ArticlelistView.as_view(), name='articles')
 ]
--- Blog/views.py
+++ Blog/views.py
@@ -1,11 +1,20 @@
-from django.shortcuts import render, get_object_or_404, redirect
+#from django.shortcuts import render, get_object_or_404, redirect
+
+from django.shortcuts import render
+from django.views.generic import (
+    CreateView,
+    DetailView,
+    UpdateView,
+    ListView,
+    DeleteView
+)
 
 from .models import Article
 from .forms import ArticleForm
 
 # Create your views here.
 
-
+"""
 def article_detail_view(request, id):
     item = Article.objects.get(id=id)
     context = {
@@ -20,3 +29,14 @@ def article_list_view(request):
         "items": items
     }
     return render(request, "Blog/article_list.html", context)
+"""
+
+
+class ArticlelistView(ListView):
+    template_name = 'Blog/article_list.html'
+    queryset = Article.objects.all()  # <blog>/<modelname>_list.html
+
+
+class ArticleDetailView(DeleteView):
+    template_name = 'blog/article_detail.html'
+    queryset = Article.objects.all()
